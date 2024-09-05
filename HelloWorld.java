34e206204ebf167374181609e9d4edfeda8b1bb9
A.J. <117821610+aattttt@users.noreply.github.com>
2024-09-05 12:20:23 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, GitHub! (Change from the future)");
+    pen.println ("Whole new line");
     pen.flush();
     pen.close();
   } // main(String[])
